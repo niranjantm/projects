396ca99e31b45f7cf9fef1c0a9fa2add3b578fe7
niranjantm <niranjantm35@gmail.com>
2023-09-26 22:02:59 +0530

    d2

--- Mini_MicroService_app/client/src/App.js
+++ Mini_MicroService_app/client/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import PostList from './components/PostList';
 import PostsCreation from './components/PostsCreation';
 
 function App() {
   return (
-    <div><h1>Blog app</h1>
+    <div className='p-5'>
+      <h1 className='text-2xl font-bold mb-2'>Blog app</h1>
+      <hr className='bg-slate-600 m-2'></hr>
     <PostsCreation></PostsCreation>
+    <PostList></PostList>
     </div>
   )
     
--- Mini_MicroService_app/client/src/components/PostsCreation.js
+++ Mini_MicroService_app/client/src/components/PostsCreation.js
@@ -1,15 +1,35 @@
 import React from 'react'
-import "../styles/PostCreation.css";
+import {useState} from "react"
+import axios from "axios";
 
 function PostsCreation() {
+const [title,setTitle] = useState("");
+const changeHandler = (event)=>{
+  console.log(title);
+  setTitle(event.target.value);
+  
+}
+const submitHandler = async (event)=>{
+  event.preventDefault();
+
+  await axios.post("http://localhost:4000/posts",{
+    title:title
+  })
+  setTitle("");
+}
   return (
-    <form>
-        <div class='main-div'>
-            <p>Create Post</p>
+    <form onSubmit={submitHandler}>
+        <div className='bg-blue-200 max-w-fit flex flex-col rounded-lg p-2'>
+            <p className='text-lg mb-2'>Create Post</p>
+            <div className="mb-2">
             <label>Enter post name</label>
-            <input></input>
-            <button type='submit' class='btn btn-Pcreate'>Create</button>
+            <input onChange={changeHandler} value={title} className='border border-gray-500 rounded-md ml-1'></input>
+            </div>
+            <div className=' min-w-full flex justify-center'>
+            <button type='submit' className="border border-gray-500 bg-red-500 max-w-fit pr-3 pl-3 pt-1 pb-1 rounded-lg">Create</button>
+            </div>
         </div>
+        
     </form>
   )
 }
