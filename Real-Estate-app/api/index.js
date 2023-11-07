ad855645f79b1c8326455b5d6c2cb348a17abbcd
niranjantm <niranjantm35@gmail.com>
2023-11-07 16:22:34 +0530

    .

--- Real-Estate-app/api/index.js
+++ Real-Estate-app/api/index.js
@@ -5,8 +5,6 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import listingRouter from "./routes/listing.routes.js";
 import cookieParser from "cookie-parser";
-import path from "path";
-// import bodyParser from "body-parser";
 
 dotenv.config();
 
@@ -19,8 +17,6 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
-
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -29,10 +25,6 @@ app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname,"client","build")));
-app.use("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"client","build","index.html"))
-})
 
 app.use((error, req, res, next) => {
   const statusCode = error.statusCode || 500;
