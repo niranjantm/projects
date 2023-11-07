fb6227100131351d2a030ff19b29d1061934893e
niranjantm <niranjantm35@gmail.com>
2023-11-07 11:38:29 +0530

    first deployment

--- Real-Estate-app/api/index.js
+++ Real-Estate-app/api/index.js
@@ -1,42 +1,49 @@
 import express from "express";
-import mongoose from "mongoose"
-import dotenv from "dotenv"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
-import authRouter from "./routes/auth.routes.js"
-import listingRouter from "./routes/listing.routes.js"
-import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth.routes.js";
+import listingRouter from "./routes/listing.routes.js";
+import cookieParser from "cookie-parser";
+import path from "path";
 // import bodyParser from "body-parser";
 
 dotenv.config();
 
-
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to mongoDB");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-//  app.use(bodyParser.json());
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
-//  app.use(cors());
- app.use("/api/users",userRouter)
- app.use("/api/auth",authRouter);
- app.use("/api/listing",listingRouter);
- app.use((error,req,res,next)=>{
-    const statusCode = error.statusCode ||500;
-    const message = error.message || "Internal server error";
-    return res.status(statusCode).json({
-        success:false,
-        errorMessage:message,
-        statusCode,
-        
-    })
- })
-
-app.listen(4000,()=>{
-    console.log("api server is live at 4000")
+app.use(express.static(path.join(__dirname,"client","build")));
+app.use("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"client","build","index.html"))
 })
+
+app.use((error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    errorMessage: message,
+    statusCode,
+  });
+});
+
+app.listen(4000, () => {
+  console.log("api server is live at 4000");
+});
