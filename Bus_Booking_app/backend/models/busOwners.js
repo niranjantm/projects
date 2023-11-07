c2d9d5e0ee51200a654ca73ac1f17cc2da63a3b6
niranjantm <niranjantm35@gmail.com>
2023-11-07 16:21:54 +0530

    .

--- Bus_Booking_app/backend/models/busOwners.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const schema = new mongoose.Schema({},{strict:false});
-
-const busOwners = mongoose.model("bus_Owner",schema);
-
-export default busOwners;
--- Bus_Booking_app/backend/models/destination.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-var thingSchema = new mongoose.Schema({}, { strict: false });
-
-const destination= mongoose.model("state_district",thingSchema);
-
-export default destination;
