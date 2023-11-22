950145531fbeb6d5fc27962c684d4f662fb25e09
niranjantm <niranjantm35@gmail.com>
2023-11-22 11:55:14 +0530

    start

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return(
+    <div style={{display:'flex',justifyItems:"center", width:"100%"}}>
+<h1 style={{margin:''}}>Portfolio</h1>
+    </div>
+    
+  ) 
+}
+
+export default App;
