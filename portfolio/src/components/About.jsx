555618cace01bf6344ba8ff9f42af789a89ea8af
niranjantm <122964949+niranjantm@users.noreply.github.com>
2024-01-29 00:52:31 +0530

    Update About.jsx

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -28,10 +28,6 @@ function About() {
             the MERN stack. Eager to apply this knowledge and contribute to
             innovative solutions in future endeavors.
           </p>
-          <p className="text-lg mb-2 text-slate-100 max-md:text-sm">
-            Thank you for visiting my portfolio, and I'm excited to collaborate with like-minded
-            professionals who share a passion for technology and innovation.
-        </p>
         </div>
         <div className="w-full flex justify-center md:ml-10 ">
           <img src="https://img.freepik.com/premium-vector/successful-business-man-dressed-stylish-suit-with-confidence-pointing-himself-with-fingers-proud-happy-high-self-esteem-concept-illustration_270158-315.jpg?w=740" alt="about-profile" className="rounded-lg w-[400px] max-md:w-[300px] shadow-lg"></img>
